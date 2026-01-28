0d7cdbdeb20fb261b2919bea6b42ee1a664a428d
Kanak Sharma <kanak4768@gmail.com>
2026-01-29 07:33:25 +1100

    updated html

--- docs/index.js
+++ docs/index.js
@@ -71,7 +71,7 @@ var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIR
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: C:\Users\kanak\AppData\Local\Temp\tmpmqa9wo8e.js
+// include: C:\Users\kanak\AppData\Local\Temp\tmp5ng021vq.js
 
   if (!Module['expectedDataFileDownloads']) Module['expectedDataFileDownloads'] = 0;
   Module['expectedDataFileDownloads']++;
@@ -206,21 +206,21 @@ Module['FS_createPath']("/res", "textures", true, true);
 
   })();
 
-// end include: C:\Users\kanak\AppData\Local\Temp\tmpmqa9wo8e.js
-// include: C:\Users\kanak\AppData\Local\Temp\tmp6y9fsvi5.js
+// end include: C:\Users\kanak\AppData\Local\Temp\tmp5ng021vq.js
+// include: C:\Users\kanak\AppData\Local\Temp\tmp2wtgov1n.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if ((typeof ENVIRONMENT_IS_WASM_WORKER != 'undefined' && ENVIRONMENT_IS_WASM_WORKER) || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD) || (typeof ENVIRONMENT_IS_AUDIO_WORKLET != 'undefined' && ENVIRONMENT_IS_AUDIO_WORKLET)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: C:\Users\kanak\AppData\Local\Temp\tmp6y9fsvi5.js
-// include: C:\Users\kanak\AppData\Local\Temp\tmpqx1c6bsr.js
+  // end include: C:\Users\kanak\AppData\Local\Temp\tmp2wtgov1n.js
+// include: C:\Users\kanak\AppData\Local\Temp\tmpirznmsxc.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: C:\Users\kanak\AppData\Local\Temp\tmpqx1c6bsr.js
+  // end include: C:\Users\kanak\AppData\Local\Temp\tmpirznmsxc.js
 
 
 var arguments_ = [];
