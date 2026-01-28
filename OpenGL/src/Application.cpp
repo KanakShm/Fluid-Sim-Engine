20876025f9b4a9db4f8939231338e32f2cb4f7c4
Kanak Sharma <kanak4768@gmail.com>
2026-01-29 10:14:24 +1100

    updated css

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -106,8 +106,8 @@ int main(void)
         int browserWidth = emscripten_run_script_int("window.innerWidth");
         int browserHeight = emscripten_run_script_int("window.innerHeight");
 
-        windowWidth = (int)(browserWidth * 0.90);
-        windowHeight = (int)(browserHeight * 0.90);
+        windowWidth = (int)(browserWidth * 0.83);
+        windowHeight = (int)(browserHeight * 0.83);
     #endif
    
     /* Initialize the library */
